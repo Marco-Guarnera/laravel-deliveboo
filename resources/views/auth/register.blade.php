a29de211475cd8a0f4d10e852b06320d21235b12
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-12-02 19:57:19 +0100

    feat: added new fields for restaurant details (name, address, PIVA, and logo) into the registration form; implemented error handling for new restaurant fields in the registration form.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,24 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
+
     <div class="row justify-content-center">
         <div class="col-md-8">
+
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
+
+                    <!-- Register form -->
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
+                        <!-- Input for restaurant name -->
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
+                            <label for="name" class="col-md-4 col-form-label text-md-end">
+                                Restaurant name *
+                            </label>
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
                                     name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- Input for restaurant address -->
+                        <div class="row mb-3">
+                            <label for="address" class="col-md-4 col-form-label text-md-end">
+                                Restaurant address *
+                            </label>
+                            <div class="col-md-6">
+                                <input id="address" type="text"
+                                    class="form-control @error('address') is-invalid @enderror" name="address"
+                                    value="{{ old('address') }}" required autocomplete="address" autofocus>
+
+                                @error('address')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- Input for restaurant piva -->
+                        <div class="row mb-3">
+                            <label for="piva" class="col-md-4 col-form-label text-md-end">
+                                Restaurant piva *
+                            </label>
+                            <div class="col-md-6">
+                                <input id="piva" type="text" class="form-control @error('piva') is-invalid @enderror"
+                                    name="piva" value="{{ old('piva') }}" required autocomplete="piva" autofocus>
+
+                                @error('piva')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <!-- Input for restaurant logo -->
+                        <div class="row mb-3">
+                            <label for="logo" class="col-md-4 col-form-label text-md-end">
+                                Restaurant logo *
+                            </label>
+                            <div class="col-md-6">
+                                <input id="logo" type="file" class="form-control @error('logo') is-invalid @enderror"
+                                    name="logo" autofocus>
+
+                                @error('logo')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
