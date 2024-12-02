94e44850d7dbeb8fb10f6e4a989d37700ec86636
Marco-Guarnera <Darkaquon93@gmail.com>
2024-12-02 15:50:16 +0100

    Created index.blade.php in views\admin\dishes

---
+++ resources/views/admin/dishes/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+<!-- Index -->
+@section('content')
+    <div class="container-fluid">
+        <!-- Table -->
+        <table class="table table-hover"></table>
+    </div>
+@endsection
