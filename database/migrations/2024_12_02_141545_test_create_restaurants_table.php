760c7a8da6548ffe8235490c1a530f3732b38760
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-12-02 20:00:29 +0100

    feat: removed useless user name in users table

--- database/migrations/2024_12_02_141545_test_create_restaurants_table.php
+++ database/migrations/2024_12_02_141545_test_create_restaurants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRestaurantsTable extends Migration
+return new class extends   Migration
 {
     public function up()
     {
@@ -23,4 +23,4 @@ class CreateRestaurantsTable extends Migration
     {
         Schema::dropIfExists('restaurants');
     }
-}
+};
