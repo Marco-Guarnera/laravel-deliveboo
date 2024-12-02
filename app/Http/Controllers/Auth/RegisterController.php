4fa958aa570a804b0a9020535ef034207d96c9ff
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-12-02 17:43:42 +0100

    feat: added validation rules for restaurant data

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
@@ -49,24 +50,36 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name' => ['required', 'string', 'max:40'],
+            'address' => ['requided', 'string', 'min:5', 'max:200'],
+            'piva' => ['requided', 'string', 'numeric', 'digits:11', ':restaurants'],
+            'logo' => ['nullable', 'image', 'max:250']
         ]);
     }
 
     /**
-     * Create a new user instance after a valid registration.
+     * Create a new user instance and associate restaurants to it after a valid registration.     *
      *
      * @param  array  $data
      * @return \App\Models\User
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+        //Create a new user
+        $user = User::create([
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        // Create the associated restaurant(s) for the user
+        $user->restaurants()->create([
+            'name' => $data['name'],
+            'address' => $data['address'],
+            'piva' => $data['piva'],
+            'logo' => $data['logo'],
+        ]);
+        return $user;
     }
 }
