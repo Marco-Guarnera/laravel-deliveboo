3d89473a478e265581ec30eda9b55538d216d7c5
Marco-Guarnera <Darkaquon93@gmail.com>
2024-12-02 16:22:33 +0100

    Added index method to DishController.php in Controllers\Admin

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dish;
 use Illuminate\Http\Request;
 
 class DishController extends Controller {
-    //
+    // Index
+    public function index() {
+        $dishes_list = Dish::all();
+        return view('admin.dishes.index', compact('dishes_list'));
+    }
 }
