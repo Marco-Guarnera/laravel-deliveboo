4fec201ac92db73b8d8a64fd9797b251ee9b9c00
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-12-03 23:17:29 +0100

    feat: manually synced restaurants with types in RestaurantTypeSeeder

--- database/seeders/RestaurantTypeSeeder.php
+++ database/seeders/RestaurantTypeSeeder.php
@@ -16,9 +16,37 @@ class RestaurantTypeSeeder extends Seeder
     {
 
         // get all restaurant
-        $restaurants = Restaurant::all();
+        $restaurantType = [
+            1 => 1, // Type ID 1: Kebab
+            2 => 2, // Type ID 2: Pizza
+            3 => 3, // Type ID 3: Chinese
+            4 => 4, // Type ID 4: Japanese
+            5 => 5, // Type ID 5: Mexican
+            6 => 6, // Type ID 6: Korean
+            7 => 7, // Type ID 7: Indian
+            8 => 8, // Type ID 8: Burger
+            9 => 9,  // Type ID 9: Steakhouse
+            10 => 10, // Type ID 10: Barbecue
+            11 => 11, // Type ID 11: Fast Food
+            12 => 12, // Type ID 12: Vegan
+            13 => 13, // Type ID 13: Gluten-Free
+        ];
 
-        // get all types
-        $types = Type::all()->pluck('id');
+        foreach ($restaurantType as $restaurantId => $typeId) {
+            // retrieve the restaurant by id
+            $restaurant = Restaurant::find($restaurantId);
+
+            // retrieve the type by id
+            $type = Type::find($typeId);
+
+            // if both the restaurant and type exist, associate them
+            if ($restaurant && $type) {
+                // sync the type to the restaurant
+                $restaurant->types()->sync([$type->id]);
+            } else {
+                // log an error message in console (cli) if the restaurant or type is not found
+                echo "error: restaurant id {$restaurantId} or type id {$typeId} not found\n";
+            }
+        }
     }
 }
