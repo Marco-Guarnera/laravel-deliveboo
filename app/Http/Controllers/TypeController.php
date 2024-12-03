26331cf1e5cc26b829e35a63aba01b10ecdbd0a1
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-12-03 16:11:16 +0100

    created type controller and added route for json

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Type;
+
 use Illuminate\Http\Request;
 
 class TypeController extends Controller
 {
-    //
+    public function index()
+    {
+        $types = Type::all();
+
+        return response()->json($types);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TypeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +23,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('types', [TypeController::class, 'index']);
