607e952d471313cee09badb555c4ca40d276c92d
Marco-Guarnera <Darkaquon93@gmail.com>
2024-12-03 15:02:46 +0100

    Updated index.blade.php in views\admin\dishes

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -4,6 +4,29 @@
 @section('content')
     <div class="container-fluid">
         <!-- Table -->
-        <table class="table table-hover"></table>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Visibility</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($dishes_list as $dish)
+                    <tr class="align-middle">
+                        <td>{{ $dish->id }}</td>
+                        <td>{{ $dish->name }}</td>
+                        <td>{{ $dish->description }}</td>
+                        <td>{{ $dish->price }}</td>
+                        <td>{{ $dish->is_visible }}</td>
+                    </tr>
+                @empty
+                    <div>Not Available</div>
+                @endforelse
+            </tbody>
+        </table>
     </div>
 @endsection
