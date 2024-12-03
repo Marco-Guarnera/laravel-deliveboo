43f521e06c84dd782dcace152e6a7f1753b425b8
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-12-03 11:50:17 +0100

    created restaurant controller

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Restaurant;
+use App\Models\Type;
+
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    //
+
+    public function store(Request $request) {}
+}
