1c68815208423c2efb4e40b38b10a86258edb120
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-12-03 23:35:18 +0100

    feat: added Api RestaurantController

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    public function index()
+    {
+        $restaurants = Restaurant::with('types', 'types.restaurants', /* 'dishes', 'dishes.restaurants' */)->paginate(10);
+
+        return response()->json([
+            'success' => true,
+            'results' => $restaurants,
+        ]);
+    }
+}
