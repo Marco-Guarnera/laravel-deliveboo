44ff92e1546e7869ba83c0f0bce38ed016ac950e
Marco-Guarnera <Darkaquon93@gmail.com>
2024-12-04 17:28:44 +0100

    Updated authorize method to DishRequest.php in Requests

--- app/Http/Requests/DishRequest.php
+++ app/Http/Requests/DishRequest.php
@@ -3,13 +3,14 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class DishRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize() : bool {
-        return true;
+        return Auth::check();
     }
 
     /**
