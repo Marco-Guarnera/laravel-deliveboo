9638d85a9ab10d15e3ba531e7caf8ee40d6e92d3
Marco-Guarnera <Darkaquon93@gmail.com>
2024-12-04 08:22:57 +0100

    Added fillable to Dish.php in Models

--- app/Models/Dish.php
+++ app/Models/Dish.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dish extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'is_visible',
+        'img'
+    ];
 }
