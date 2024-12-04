78565536f009e147d570a67ee9a1c7e8f3c82f25
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-04 22:23:13 +0100

    feat: associate dishes with user's restaurant in DishController, store and index methods

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -8,20 +8,28 @@ use App\Models\Dish;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-class DishController extends Controller {
-    public function __construct() {
+class DishController extends Controller
+{
+    public function __construct()
+    {
         $this->middleware('auth');
     }
 
     // Create
-    public function create() {
+    public function create()
+    {
         $dish = new Dish();
         return view('admin.dishes.create', compact('dish'));
     }
 
     // Store
-    public function store(DishRequest $request) {
+    public function store(DishRequest $request)
+    {
         $data_list = $request->validated();
+
+        $restaurant_id = auth()->user()->restaurant->id;
+
+        $data_list['restaurant_id'] = $restaurant_id;
 
         if ($request->hasFile('img')) {
             $file_path = Storage::disk('public')->put('img/dishes/', $request->img);
@@ -34,23 +42,34 @@ class DishController extends Controller {
     }
 
     // Index
-    public function index() {
-        $dishes_list = Dish::paginate(10);
-        return view('admin.dishes.index', compact('dishes_list'));
+    public function index()
+    {
+
+        // get the currently authenticated user
+        $user = auth()->user();
+
+        // retrieve the user's restaurants with their related dishes using eager loading
+        $restaurants = $user->restaurants()->with('dishes')->get();
+
+        // pass the retrieved data (restaurants and their dishes) to the view
+        return view('admin.dishes.index', compact('restaurants'));
     }
 
     // Show
-    public function show(Dish $dish) {
+    public function show(Dish $dish)
+    {
         return view('admin.dishes.show', compact('dish'));
     }
 
     // Edit
-    public function edit(Dish $dish) {
+    public function edit(Dish $dish)
+    {
         return view('admin.dishes.edit', compact('dish'));
     }
 
     // Update
-    public function update(DishRequest $request, Dish $dish) {
+    public function update(DishRequest $request, Dish $dish)
+    {
         $data_list = $request->validated();
 
         if ($request->hasFile('img')) {
@@ -65,7 +84,8 @@ class DishController extends Controller {
     }
 
     // Delete
-    public function destroy(Dish $dish) {
+    public function destroy(Dish $dish)
+    {
         $dish->delete();
         return redirect()->route('admin.dishes.index');
     }
