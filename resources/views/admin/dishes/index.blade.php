32cd5dc87459ce0699ee8de81d9906b6583b5f96
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-04 22:43:15 +0100

    feat: display dishes associated with the current user's authenticated restaurant

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -2,43 +2,45 @@
 
 <!-- Index -->
 @section('content')
-    <div class="container-fluid">
-        <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary mb-3">Create</a>
-        <!-- Table -->
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <th>Visibility</th>
-                    <th>Functions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($dishes_list as $dish)
-                    <tr class="align-middle">
-                        <td>{{ $dish->id }}</td>
-                        <td>{{ $dish->name }}</td>
-                        <td>{{ $dish->description }}</td>
-                        <td>{{ $dish->price }}</td>
-                        <td>{{ $dish->is_visible }}</td>
-                        <td>
-                            <a href="{{ route('admin.dishes.show', $dish) }}" class="btn btn-success">Show</a>
-                            <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-warning">Edit</a>
-                            <form action="{{ route('admin.dishes.delete', $dish) }}" method="post" class="d-inline">
-                                @method('delete')
-                                @csrf
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @empty
-                    <div>Not Available</div>
-                @endforelse
-            </tbody>
-        </table>
-        <div>{{ $dishes_list->links() }}</div>
-    </div>
+<div class="container-fluid">
+    <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary mb-3">Create</a>
+    <!-- Table -->
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Visibility</th>
+                <th>Functions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($restaurants as $restaurant)
+            @forelse ($restaurant->dishes as $dish)
+            <tr class="align-middle">
+                <td>{{ $dish->id }}</td>
+                <td>{{ $dish->name }}</td>
+                <td>{{ $dish->description }}</td>
+                <td>{{ $dish->price }}</td>
+                <td>{{ $dish->is_visible }}</td>
+                <td>
+                    <a href="{{ route('admin.dishes.show', $dish) }}" class="btn btn-success">Show</a>
+                    <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-warning">Edit</a>
+                    <form action="{{ route('admin.dishes.delete', $dish) }}" method="post" class="d-inline">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @empty
+            <div>Not Available</div>
+            @endforelse
+            @endforeach
+        </tbody>
+    </table>
+    <div></div>
+</div>
 @endsection
