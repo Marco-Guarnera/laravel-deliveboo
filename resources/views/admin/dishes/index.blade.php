916d1c78b5c548ad9beb43ae0a0eddf1b10d89b9
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-12-05 17:01:16 +0100

    made modal work in dish show section

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -45,14 +45,7 @@ foreach ($restaurants as $restaurant) {
                 <td>
                     <a href="{{ route('admin.dishes.show', $dish) }}" class="btn btn-success">Show</a>
                     <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-warning">Edit</a>
-                    <form action="{{ route('admin.dishes.delete', $dish) }}" method="post" class="d-inline">
-                        @method('delete')
-                        @csrf
-                    <!--Button that triggers modal-->
-                        <button type="button" class="btn btn-danger modalTrigger" data-bs-toggle="modal"
-                        data-bs-target="#deleteModal"
-                        data-dish-id="{{ $dish->id }}" data-dish-name="{{ $dish->name }}">Delete</button>
-                    </form>
+
                 </td>
             </tr>
             @empty
@@ -69,49 +62,9 @@ foreach ($restaurants as $restaurant) {
     @endif
 
 </div>
-<!--Modal for delete confirmation-->
 
 
-  <!-- Modal -->
-  <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModal" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Delete confirmation</h5>
-          <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <div class="modal-body">
-        Are you sure you want to delete this dish? This operation is not reversible.
-    </div>
-    <div class="modal-footer">
-
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keep this dish</button>
-        <!--Delete form-->
-          <form id="deleteForm" method="POST" action="" style="display: inline;">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Delete</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
 @endsection
 
-@section('modalScript')
-
-
-document.addEventListener('DOMContentLoaded', function() {
-const modalTriggers = document.querySelectorAll('modalTrigger');
-
-<!--const dishName = this.getAttribute('data-dish-name');
-const dishId = this.getAttribute('data-dish-id');-->
-
-console.log('ok');
-
-});
-
-@endsection
 
 
