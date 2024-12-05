3b1077b84f8651633b360182d5d3780268be42b1
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-12-05 18:51:45 +0100

    fixed route error

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -4,101 +4,90 @@
 @section('content')
 <div class="container-fluid">
 
-
-<!--Messaggio in caso non ci siano piatti-->
-@php
-
-$hasDishes = false;
-foreach ($restaurants as $restaurant) {
-    if ($restaurant->dishes->isNotEmpty()) {
-        $hasDishes = true;
-        break;
+    <!-- Messaggio in caso non ci siano piatti -->
+    @php
+    $hasDishes = false;
+    foreach ($restaurants as $restaurant) {
+        if ($restaurant->dishes->isNotEmpty()) {
+            $hasDishes = true;
+            break;
+        }
     }
-}
-@endphp
+    @endphp
 
+    @if ($hasDishes)
+        <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary mb-3">Add a new dish!</a>
 
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Visibility</th>
+                    <th>Functions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($restaurants as $restaurant)
+                    @forelse ($restaurant->dishes as $dish)
+                        <tr class="align-middle">
+                            <td>{{ $dish->id }}</td>
+                            <td>{{ $dish->name }}</td>
+                            <td>{{ $dish->description }}</td>
+                            <td>{{ $dish->price }}</td>
+                            <td>{{ $dish->is_visible }}</td>
+                            <td>
+                                <a href="{{ route('admin.dishes.show', $dish) }}" class="btn btn-success">Show</a>
+                                <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-warning">Edit</a>
+                                <!-- Delete button -->
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                        data-bs-target="#deleteModal{{ $dish->id }}">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
 
-@if ($hasDishes)
-<a href="{{ route('admin.dishes.create') }}" class="btn btn-primary mb-3">Add a new dish!</a>
-    <!-- Table -->
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Price</th>
-                <th>Visibility</th>
-                <th>Functions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($restaurants as $restaurant)
-            @forelse ($restaurant->dishes as $dish)
-            <tr class="align-middle">
-                <td>{{ $dish->id }}</td>
-                <td>{{ $dish->name }}</td>
-                <td>{{ $dish->description }}</td>
-                <td>{{ $dish->price }}</td>
-                <td>{{ $dish->is_visible }}</td>
-                <td>
-                    <a href="{{ route('admin.dishes.show', $dish) }}" class="btn btn-success">Show</a>
-                    <a href="{{ route('admin.dishes.edit', $dish) }}" class="btn btn-warning">Edit</a>
-                     <!--Delete button-->
-                     <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                     data-bs-target="#deleteModal"
-                     data-dish-id="{{ $dish->id }}"
-                     data-dish-name="{{ $dish->name }}">
-                     Delete
-                 </button>
+                        <!-- Modal for delete -->
+                        <div class="modal fade" id="deleteModal{{ $dish->id }}" tabindex="-1" aria-labelledby="deleteModalLabel{{ $dish->id }}" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="deleteModalLabel{{ $dish->id }}">Delete Confirmation</h5>
+                                    </div>
+                                    <div class="modal-body">
+                                        Are you sure you want to delete this dish? This operation is not reversible.
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keep this dish</button>
 
-                </td>
-            </tr>
-            @empty
-            <div>Not Available</div>
-            @endforelse
-            @endforeach
-        </tbody>
-    </table>
+                                        <!-- Delete form -->
+                                        <form method="POST" action="{{ route('admin.dishes.delete', $dish) }}" style="display: inline;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Delete</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    @empty
+                        <div>Not Available</div>
+                    @endforelse
+                @endforeach
+            </tbody>
+        </table>
     @else
-   <div class="text-center mt-4">
-    <p>No dishes available. Start inserting your dishes!</p>
-    <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary mb-3">Add a new dish!</a>
-  </div>
+        <div class="text-center mt-4">
+            <p>No dishes available. Start inserting your dishes!</p>
+            <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary mb-3">Add a new dish!</a>
+        </div>
     @endif
 
 </div>
-
-
-
-
-
-<!--Delete modal -->
-<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Delete Confirmation</h5>
-            </div>
-            <div class="modal-body">
-                Are you sure you want to delete this dish? This operation is not reversible.
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keep this dish</button>
-
-                <!-- Delete form -->
-                <form id="deleteForm" method="POST"  action="{{ route('admin.dishes.delete', $dish) }}" style="display: inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
-
 @section('scripts')
 
 
