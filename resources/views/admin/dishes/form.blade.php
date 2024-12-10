96fc22931b9d0ff6c2c900fc749049ee01b20663
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-10 12:58:37 +0100

    feat: add id to get form element in js

--- resources/views/admin/dishes/form.blade.php
+++ resources/views/admin/dishes/form.blade.php
@@ -14,23 +14,28 @@
         </div>
     </div>
     <div class="row w-100 justify-content-center">
-        <d class="col-12 col-md-8 col-lg-6">
+        <div class="col-12 col-md-8 col-lg-6">
             <!-- Form -->
-            <form action="@yield('form-action')" method="post" enctype="multipart/form-data">
+            <form id="dishes-form" action="@yield('form-action')" method="post" enctype="multipart/form-data">
                 @yield('form-method')
                 @csrf
+
                 <!-- Dish Name -->
                 <label for="dish-name" class="form-label">Name:</label>
                 <input type="text" id="dish-name" class="form-control" name="name"
-                    value="{{ old('name', $dish->name) }}" placeholder="Name">
+                    value="{{ old('name', $dish->name) }}" placeholder="Name" minlength="5" maxlength="250" required>
+
                 <!-- Dish Description -->
                 <label for="dish-description" class="form-label">Description:</label>
-                <textarea id="dish-description" class="form-control" name="description" rows="5"
-                    placeholder="Description">{{ old('description', $dish->description) }}</textarea>
+                <textarea id="dish-description" class="form-control" name="description"
+                    placeholder="Description (optional)" minlength="15"
+                    maxlength="500">{{ old('description', $dish->description) }}</textarea>
+
                 <!-- Dish Price -->
                 <label for="dish-price" class="form-label">Price:</label>
                 <input type="number" id="dish-price" class="form-control" name="price"
-                    value="{{ old('price', $dish->price) }}" min="0" max="100" step="0.01" placeholder="€">
+                    value="{{ old('price', $dish->price) }}" min="0" max="100" step="0.01" placeholder="€" required>
+
                 <!-- Dish Visibility -->
                 <div class="form-check  my-3">
                     <label for="dish-visibility" class="form-label">Visibility:</label>
@@ -40,16 +45,18 @@
                     </select>
                 </div>
 
+
                 <!-- Dish Img -->
                 <label for="dish-img" class="form-label">Image:</label>
-                <input type="file" id="dish-img" class="form-control" name="img">
-                <!-- Btns -->
+                <input type="file" id="dish-img" class="form-control" name="img" accept="image/*">
+
+                <!-- Buttons -->
                 <div class="my-3">
                     <button type="submit" class="btn btn-primary">@yield('form-submit')</button>
                     <button type="reset" class="btn btn-secondary">Reset</button>
                 </div>
             </form>
-        </d iv>
+        </div>
     </div>
 </div>
 @endsection
