0efdb74ddbc3de7443af364e36419cf95b9bd010
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-10 01:15:20 +0100

    feat: initialize dishes form validation elements and inputs

---
+++ resources/js/dishes-validation.js
@@ -0,0 +1,12 @@
+console.log('Dishes JS OK');
+
+// !DISHES FORM
+
+// |GET INTERESTED DOCUMENT ELEMENTS
+// Form and input fields for dishes
+const dishesForm = document.getElementById('dishes-form');
+const dishName = document.getElementById('dish-name'); // Name input
+const dishDescription = document.getElementById('dish-description'); // Description input
+const dishPrice = document.getElementById('dish-price'); // Price input
+const dishVisibility = document.getElementById('dish-visibility'); // Visibility checkbox
+const dishImg = document.getElementById('dish-img'); // Image input
