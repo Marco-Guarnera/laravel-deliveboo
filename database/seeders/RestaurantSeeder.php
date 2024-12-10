2929a3c4cd0f6bacdc406c059145983f42648ae1
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-10 19:11:29 +0100

    feat: Implemented RestaurantSeeder to populate restaurants from CSV file

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Restaurant;
-use App\Models\Type;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class RestaurantSeeder extends Seeder
@@ -12,5 +10,24 @@ class RestaurantSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void {};
+    public function run(): void
+    {
+        $filePath = database_path('seeders/csvs/restaurants_data.csv');
+        $file = fopen($filePath, 'r');
+        $tableColumns = fgetcsv($file);
+
+        while (($row = fgetcsv($file)) !== false) {
+            $restaurantData = [
+                'name' => $row[array_search('name', $tableColumns)],
+                'address' => $row[array_search('address', $tableColumns)],
+                'piva' => $row[array_search('piva', $tableColumns)],
+                'logo' => $row[array_search('logo', $tableColumns)],
+                'user_id' => $row[array_search('user_id', $tableColumns)],
+            ];
+
+            Restaurant::create($restaurantData);
+        }
+
+        fclose($file);
+    }
 }
