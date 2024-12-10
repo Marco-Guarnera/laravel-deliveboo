e626c6f579b4c813756efdb456a08095e8261398
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-12-10 16:47:03 +0100

    created Order model and index in OrderController

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Order;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    // Index
+    public function index()
+    {
+        $order_list = Order::all();
+        return response()->json(
+            [
+                'success' => true,
+                'results' => $order_list
+            ]
+        );
+    }
 }
