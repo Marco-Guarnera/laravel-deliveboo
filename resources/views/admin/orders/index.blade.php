f4b546cae1277d7d9b93b5b68c5c58019171ec6e
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-12-11 16:18:31 +0100

    no orders on orders index index

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -3,13 +3,10 @@
 @section('content')
 
 <div class="container-fluid">
+    <div class="d-flex flex-column align-items-center justify-content-center">
+    <h3 class="text-center">Your Restaurant hasn't any orders yet!</h3>
     <a href="{{ route('admin.dishes.index') }}" class="btn btn-primary mb-3">Go back</a>
-
-
-
-
-
-
+    </div>
 </div>
 
 
