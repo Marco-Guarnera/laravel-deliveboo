cb2dde20eb0f617847495e16f05dca7c2727ebb1
Marco-Guarnera <Darkaquon93@gmail.com>
2024-12-11 12:37:28 +0100

    Added fillable to Order.php in Models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
-{
+class Order extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_name',
+        'customer_email',
+        'customer_number',
+        'customer_address'
+    ];
 }
