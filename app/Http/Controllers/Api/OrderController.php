22d7db180e4750ee8bd1a94fa9da21053e75afd8
Marco-Guarnera <Darkaquon93@gmail.com>
2024-12-11 14:41:50 +0100

    Added store method to OrderController.php in Controllers\Api

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,18 +5,39 @@ namespace App\Http\Controllers\Api;
 use App\Models\Order;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
-class OrderController extends Controller
-{
+class OrderController extends Controller {
+    // Store
+    public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'customer_name' => ['required', 'string', 'min:2', 'max:250'],
+            'customer_email' => ['required', 'email', 'min:6', 'max:250'],
+            'customer_number' => ['required', 'numeric', 'integer', 'size:10'],
+            'customer_address' => ['required', 'string', 'min:10', 'max:250']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        } else {
+            $order = Order::create($validator->validated());
+            Mail::to('admin@gmail.com')->send(new Order($order));
+            return response()->json([
+                'success' => true
+            ]);
+        }
+    }
+
     // Index
-    public function index()
-    {
+    public function index() {
         $orders_list = Order::all();
-        return response()->json(
-            [
+        return response()->json([
                 'success' => true,
                 'results' => $orders_list
-            ]
-        );
+            ]);
     }
 }
