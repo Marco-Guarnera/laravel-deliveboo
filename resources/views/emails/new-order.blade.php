9e7f263afb8bdf39757146f890da2a0e7fd1bb47
Marco-Guarnera <Darkaquon93@gmail.com>
2024-12-11 19:25:41 +0100

    Fixed new-order.blade.php in views\emails

--- resources/views/emails/new-order.blade.php
+++ resources/views/emails/new-order.blade.php
@@ -4,9 +4,9 @@
 @section('content')
     <h2>New order</h2>
     <ul>
-        <li>Name: {{ $order->name }}</li>
-        <li>Email: {{ $order->email }}</li>
-        <li>Number: {{ $order->number }}</li>
-        <li>Address: {{ $order->address }}</li>
+        <li>Name: {{ $order->customer_name }}</li>
+        <li>Email: {{ $order->customer_email }}</li>
+        <li>Number: {{ $order->customer_number }}</li>
+        <li>Address: {{ $order->customer_address }}</li>
     </ul>
 @endsection
