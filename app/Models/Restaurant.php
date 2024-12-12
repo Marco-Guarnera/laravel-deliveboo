2b35dee76bdcf9e0379c1ac1ea7f6f896e50aa9b
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-12 12:08:02 +0100

    feat: integrated slug creation logic into Restaurant model

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Restaurant extends Model
 {
@@ -11,12 +12,21 @@ class Restaurant extends Model
 
     protected $fillable = [
         'name',
+        'slug',
         'address',
         'piva',
         'logo',
         'user_id'
     ];
 
+    /**
+     * Mutator to automatically generate the slug from the  restaurant name.
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value, '-');
+    }
 
     /**
      * Get the user that owns the restaurant.
@@ -26,12 +36,17 @@ class Restaurant extends Model
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Relationship with the associated types.
+     */
     public function types()
     {
         return $this->belongsToMany(Type::class);
     }
 
-
+    /**
+     * Relationship with the associated dishes.
+     */
     public function dishes()
     {
         return $this->hasMany(Dish::class);
