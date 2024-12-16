0b2ed49b10a7e558a467aa638f560762c72a4bdf
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-12-16 16:14:46 +0100

    feat: import orders data from orders_data cvs file

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $filePath = database_path('seeders/csvs/orders_data.csv');
+        $file = fopen($filePath, 'r');
+        $tableColumns = fgetcsv($file);
+
+        while (($row = fgetcsv($file)) !== false) {
+            $orderData = [
+                'id' => $row[array_search('id', $tableColumns)],
+                'restaurant_id' => $row[array_search('restaurant_id', $tableColumns)],
+                'customer_name' => $row[array_search('customer_name', $tableColumns)],
+                'customer_email' => $row[array_search('customer_email', $tableColumns)],
+                'customer_number' => $row[array_search('customer_number', $tableColumns)],
+                'customer_address' => $row[array_search('customer_address', $tableColumns)],
+                'total_price' => $row[array_search('total_price', $tableColumns)],
+                'created_at' => $row[array_search('created_at', $tableColumns)],
+            ];
+            Order::create($orderData);
+        }
+        fclose($file);
     }
 }
