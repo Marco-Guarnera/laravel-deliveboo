7180d31967849e232d30ee97d5da84e1f2f791d2
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-12-17 17:13:35 +0100

    refactoring welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,39 +22,32 @@
 
             <h3 class="text-dark fw-bold">DeliveBoo - Administration Area</h3>
 
-            @if (Route::has('login'))
-
-                <div class="d-flex align-items-center">
-                    @auth
-
-
-                        <a class="btn btn-danger text-white ms-3" href="{{ route('admin.dishes.index') }}">Your Restaurant's page</a>
-                    @else
-
-                        <a class="btn btn-success text-white ms-3" href="{{ route('login') }}">Log in</a>
-
-                        @if (Route::has('register'))
-
-                            <a class="btn btn-danger text-white ms-3" href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-            </main>
+        </main>
 
 
 
-        </div>
-    </body>
+    </div>
+</body>
 </header>
 <main id="welcome-main">
     <div class="mt-5">
-   <h1 class="text-center">Join us and start managing your business! </h1>
-   <h2 class="text-center">Discover your restaurant's statistics and personalize your menu.</h2>
+        <h1 class="text-center">Join us and start managing your business! </h1>
+        <h2 class="text-center">Discover your restaurant's statistics and personalize your menu.</h2>
+        @if (Route::has('login'))
+            <div class="d-flex justify-content-center align-items-center gap-3 mt-4">
+                @auth
+                    <a class="btn btn-danger btn-lg text-white" href="{{ route('admin.dishes.index') }}">Your Restaurant's page</a>
+                @else
+                    <a class="btn btn-success btn-lg text-white" href="{{ route('login') }}">Log in</a>
 
-
-        </body>
-        </html>
+                    @if (Route::has('register'))
+                        <a class="btn btn-danger btn-lg text-white" href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
+</main>
 
 
 </html>
